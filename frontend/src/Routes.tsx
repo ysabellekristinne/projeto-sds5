6982aa54a5914e609a16d4bcbe9030a144f191e4
Ysabelle Kristinne <ysabellekristinne@gmail.com>
2021-11-06 15:53:37 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,21 @@
+import Dashboard from 'assets/pages/Dashboard';
+import Home from 'assets/pages/Home';
+import { BrowserRouter , Route, Switch} from 'react-router-dom';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashboard" >
+                    <Dashboard />
+                </Route>
+            </Switch>
+
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
